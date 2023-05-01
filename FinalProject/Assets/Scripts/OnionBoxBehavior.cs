edf7551f86745da14388308e917bf20d01ec16af
NatalieMK9 <64727532+NatalieMK9@users.noreply.github.com>
2023-05-01 14:02:42 -0400

    5/1 2:00

--- FinalProject/Assets/Scripts/OnionBoxBehavior.cs
+++ FinalProject/Assets/Scripts/OnionBoxBehavior.cs
@@ -1,31 +1,40 @@
 ﻿/*
- * This is the code that activates when the red key is near the lock to summon the keycard
- * When the collider for the red key comes into contact with the lock's collider, it summons a keycard at the lock.
- * This code also makes sure only one keycard is summoned
+ * Onion minigame tracker
  */
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OnionBoxBehavior : MonoBehaviour
 {
-    public GameObject DebitCard;
-    public GameObject card;
+    public GameObject jarst_onion;
+    public GameObject onion;
+    public int score = 0;
+    public Text MinigameText;
+    public float countdown = 6;
 
     void OnTriggerEnter(Collider other)
     {
-        //summon makes sure that only one keycard appears
-        if (other.name == "redkey")
+        //when onion enters box, make new onion
+        if (other.name == "onionshelf")
         {
-            Debug.Log("Hand is in the Collision Box");
-            CreateDebitCard();
-            card.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-            card.transform.position = new Vector3(7.94f, 1.413f, 6.843f);
+            Debug.Log("onion in the box");
+            NewOnion();
+            onion.transform.localScale = new Vector3(1.533f, 1.533f, 1.533f);
+            onion.transform.position = new Vector3(0.21f, 0.15f, 0.1f);
+            score++;
+            Debug.Log(score);
+            countdown -= Time.deltaTime;
+            if (countdown > 0) {
+                MinigameText.text = "Restocking! How fast can you put the right number of items in their right basket?";
+            }
+            MinigameText.text = (string.Format("{0:N2}", score));
         }
     }
-    public void CreateDebitCard()
+    public void NewOnion()
     {
-        card = Instantiate(DebitCard);
+        onion = Instantiate(jarst_onion);
     }
 }
